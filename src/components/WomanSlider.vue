4b66433c38b7ca927b433d2ce6901b8da84ad5aa
Hairul Jr <januarhairul@gmail.com>
2021-09-24 23:25:30 +0700

    Build, Feature : Install Axios & Implementasi API Products

--- src/components/WomanSlider.vue
+++ src/components/WomanSlider.vue
@@ -3,7 +3,7 @@
   <section class="women-banner spad">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-12 mt-5">
+        <div class="col-lg-12 mt-5" v-if="products.length > 0">
           <carousel
             class="product-slider"
             :nav="false"
@@ -12,7 +12,11 @@
             :margin="25"
             :dots="false"
           >
-            <div class="product-item">
+            <div
+              class="product-item"
+              v-for="product in products"
+              :key="product.id"
+            >
               <div class="pi-pic">
                 <img src="img/mickey1.jpg" alt="" />
                 <ul>
@@ -25,74 +29,20 @@
                 </ul>
               </div>
               <div class="pi-text">
-                <div class="catagory-name">Coat</div>
+                <div class="catagory-name">{{ product.type }}</div>
                 <router-link to="/product">
-                  <h5>Mickey Baggy</h5>
+                  <h5>{{ product.name }}</h5>
                 </router-link>
                 <div class="product-price">
-                  $14.00
-                  <span>$35.00</span>
+                  {{ rupiah(product.price) }}
+                  <!-- <span>$35.00</span> -->
                 </div>
               </div>
             </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-2.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Shoes</div>
-                <a href="#">
-                  <h5>Guangzhou sweater</h5>
-                </a>
-                <div class="product-price">$13.00</div>
-              </div>
-            </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-3.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Pure Pineapple</h5>
-                </a>
-                <div class="product-price">$34.00</div>
-              </div>
-            </div>
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-4.jpg" alt="" />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
-                  </li>
-                  <li class="quick-view"><a href="#">+ Quick View</a></li>
-                  <li class="w-icon">
-                    <a href="#"><i class="fa fa-random"></i></a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Converse Shoes</h5>
-                </a>
-                <div class="product-price">$34.00</div>
-              </div>
-            </div>
           </carousel>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-12" v-else>
+          <p>Produk terbaru belum tersedia untuk saat ini.</p>
         </div>
       </div>
     </div>
@@ -102,10 +52,28 @@
 
  <script>
 import carousel from "vue-owl-carousel";
+import axios from "axios";
 export default {
   name: "WomanSlider",
   components: {
     carousel,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://127.0.0.1:8000/api/products")
+      .then((res) => (this.products = res.data.data.data))
+      .catch((err) => console.log(err));
+  },
+  methods: {
+    rupiah(value) {
+      let val = (value / 1).toFixed(2).replace(".", ",");
+      return "Rp. " + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    },
   },
 };
 </script>
